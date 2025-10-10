7cca0cbbb62207e2146da54af7d1951dcd6bb7fa
Matheus Ruivo <matheushenriquerodriguesruivo4@gmail.com>
2025-10-10 00:54:37 -0300

    Add files via upload

---
+++ banco.py
@@ -0,0 +1,17 @@
+#Caixa eletrônico
+#Autor: Matheus Ruivo
+print("Bem vindo(A) ao caixa eletrônico.")
+entrada = input("Quer entrar ou sair?")
+if entrada == 'entrar':
+    conta = float(input("Quanto de dinheiro tem na sua conta?"))
+    print("Carregando...")
+    conta2 = float(input("Quanto quer sacar?"))
+    soma = conta - conta2
+    print("Carregando...")
+    print(f"Pronto, saque com sucesso! sobrou: {soma}")
+    print("Volte sempre!")
+elif entrada == 'sair':
+    print("Até a proxima!")
+else:
+    print("Você não entrou e nem saiu!! reveja!") 
+
